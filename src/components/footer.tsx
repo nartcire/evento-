4e63c8357aae22b86183ad4a7c0218bc5c6a6461
Eric Tran <erictran1547@gmail.com>
2025-04-03 19:58:48 -0700

    Still on video 203

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,28 @@
+import Link from "next/link";
+
+const routes = [
+  {
+    path: "/terms-conditions",
+    name: "Terms & Conditions",
+  },
+  {
+    path: "/privacy-policy",
+    name: "Privacy Policy",
+  },
+];
+
 export default function Footer() {
-  return <footer className="mt-auto">footer</footer>;
+  return (
+    <footer className="mt-auto">
+      <small>&copy; 2050 EricTran. All rights reserved.</small>
+
+      <ul>
+        {routes.map((route) => (
+          <li key={route.path}>
+            <Link href={route.path}>{route.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </footer>
+  );
 }
