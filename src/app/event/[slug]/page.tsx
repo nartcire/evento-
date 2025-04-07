0f14e3bccd534d0b98b2aaccc50d0f60ae0b7935
Eric Tran <erictran1547@gmail.com>
2025-04-06 17:44:56 -0700

    Finished Video 233

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -54,12 +54,40 @@ export default async function EventPage({ params }: EventPageProps) {
               Organized by <span className="italic">{event.organizerName}</span>
             </p>
 
-            <button className="bg-white/20 text-lg capitalize bg-blur mt-5 lg:mt-auto w-[95vw] rounded-md border-white/10 border-2 sm:w-full py-2 hover:scale-105 focus:scale-105 active:scale-[1.02] transition">
+            <button className="bg-white/20 text-lg capitalize bg-blur mt-5 lg:mt-auto w-[95vw] rounded-md border-white/10 border-2 sm:w-full py-2 state-effects">
               Get tickets
             </button>
           </div>
         </div>
       </section>
+
+      <div className="min-h-[75vh] text-center px-5 py-16">
+        <Section>
+          <SectionHeading>About this event</SectionHeading>
+          <SectionContent>{event.description}</SectionContent>
+        </Section>
+
+        <Section>
+          <SectionHeading>Location</SectionHeading>
+          <SectionContent>{event.location}</SectionContent>
+        </Section>
+      </div>
     </main>
+  );
+}
+
+function Section({ children }: { children: React.ReactNode }) {
+  return <section className="mb-12">{children}</section>;
+}
+
+function SectionHeading({ children }: { children: React.ReactNode }) {
+  return <h2 className="text-2xl mb-8">{children}</h2>;
+}
+
+function SectionContent({ children }: { children: React.ReactNode }) {
+  return (
+    <p className="max-w-4xl mx-auto text-lg leading-8 text-white/75">
+      {children}
+    </p>
   );
 }
